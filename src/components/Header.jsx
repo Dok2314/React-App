12629e9df8606d90d3bf853e65271478a177aa38
Dok2314 <koversundanil@gmail.com>
2023-03-30 23:11:31 +0300

    Added .module.css extension to file and add classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.css';
+import headerCss from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={headerCss.header}>
             <img src="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png" alt="Logo"/>
         </header>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
-import './Profile.css';
+import profileCss from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={profileCss.content}>
             <div>
                 <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
             </div>
@@ -14,14 +14,14 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={profileCss.item}>
+                    <div className={profileCss.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className={profileCss.item}>
                         post 2
                     </div>
-                    <div>
+                    <div className={profileCss.item}>
                         post 3
                     </div>
                 </div>
