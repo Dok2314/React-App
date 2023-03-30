c7a21fe4b9da4c65fb164296de8a9bfdc857f9cd
Dok2314 <koversundanil@gmail.com>
2023-03-30 22:26:20 +0300

    Added components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png" alt="Logo"/>
+        </header>
+    );
+};
+
+export default Header;
