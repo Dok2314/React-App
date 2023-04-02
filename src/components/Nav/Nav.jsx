4a3fee82d223979357403133bc53fd0d8096a8f8
Dok2314 <koversundanil@gmail.com>
2023-04-02 15:31:33 +0300

    Added active linkg

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,20 +4,20 @@ import {NavLink} from "react-router-dom";
 const Nav = () => {
     return (
         <nav className={navCss.nav}>
-            <div className={`${navCss.item} ${navCss.active}`}>
-                <NavLink to="/profile">Profile</NavLink>
+            <div className={navCss.item}>
+                <NavLink to="/profile" className={navData => navData.isActive ? navCss.activeLink : navCss.item}>Profile</NavLink>
             </div>
             <div className={navCss.item}>
-                <NavLink to="/dialogs">Messages</NavLink>
+                <NavLink to="/dialogs" className={navData => navData.isActive ? navCss.activeLink : navCss.item}>Messages</NavLink>
             </div>
             <div className={navCss.item}>
-                <NavLink to="/news">News</NavLink>
+                <NavLink to="/news" className={navData => navData.isActive ? navCss.activeLink : navCss.item}>News</NavLink>
             </div>
             <div className={navCss.item}>
-                <NavLink to="/music">Music</NavLink>
+                <NavLink to="/music" className={navData => navData.isActive ? navCss.activeLink : navCss.item}>Music</NavLink>
             </div>
             <div className={navCss.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings" className={navData => navData.isActive ? navCss.activeLink : navCss.item}>Settings</NavLink>
             </div>
         </nav>
     );
