ece9d2636312facae159c899654427a91d0b3570
Dok2314 <koversundanil@gmail.com>
2023-04-02 14:54:37 +0300

    Added NavLink tag to open page without reload

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,23 @@
 import navCss from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className={navCss.nav}>
             <div className={`${navCss.item} ${navCss.active}`}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={navCss.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={navCss.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={navCss.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={navCss.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings">Settings</NavLink>
             </div>
         </nav>
     );
