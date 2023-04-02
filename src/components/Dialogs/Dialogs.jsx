c9841a9be650c125a83731ca7b48482572c9a03e
Dok2314 <koversundanil@gmail.com>
2023-04-02 16:20:17 +0300

    Added styles to dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
 import dialogsCss from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={dialogsCss.dialogs}>
+            <div className={dialogsCss.dialogsItems}>
+                <div className={dialogsCss.dialog + ' ' + dialogsCss.active}>
+                    <NavLink to="/dialogs/1">Daniil</NavLink>
+                </div>
+                <div className={dialogsCss.dialog}>
+                    <NavLink to="/dialogs/2">Dimych</NavLink>
+                </div>
+                <div className={dialogsCss.dialog}>
+                    <NavLink to="/dialogs/3">Oleg</NavLink>
+                </div>
+            </div>
+            <div className={dialogsCss.messages}>
+                <div className={dialogsCss.message}>Hi</div>
+                <div className={dialogsCss.message}>How is your it-kamasutra</div>
+                <div className={dialogsCss.message}>Yo</div>
+            </div>
         </div>
     );
 };
