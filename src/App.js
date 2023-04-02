4ac8052ced5ac5e7e80fca6e69bf3276e29aa563
Dok2314 <koversundanil@gmail.com>
2023-04-02 14:50:58 +0300

    Added components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import dialogsCss from "./components/Dialogs/Dialogs.module.css";
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
@@ -18,6 +20,9 @@ const App = () => {
                 <Routes>
                     <Route path='/dialogs' element={<Dialogs />} />
                     <Route path='/profile' element={<Profile />} />
+                    <Route path='/news' element={<News />} />
+                    <Route path='/music' element={<Music />} />
+                    <Route path='/settings' element={<Settings />} />
                 </Routes>
             </div>
         </div>
---
+++ src/components/News/News.jsx
@@ -0,0 +1,11 @@
+import newsCss from './News.module.css';
+
+const News = (props) => {
+    return (
+        <div>
+            News
+        </div>
+    );
+};
+
+export default News;
